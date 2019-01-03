8da0e379e9435d4b03937b6eaf199fff90cc232e
Dion baharudin <45952796+Dionese@users.noreply.github.com>
2019-01-03 13:09:13 +0700

    Add files via upload

---
+++ 1.menyalin isi array A ke array B.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+    int c[5]={5,10,6,0,4};
+    int d[5];
+    int i=0,j=0;
+    printf("Nillai array yang akan di tampilkan\n");
+    for(i=4;i>=0;i--)
+    {
+        d[i]=c[j];
+        printf("%d",c[j]);
+        j++;
+    }
+    printf("\n");
+    printf("Hasil salinan dari Array A sebagai berikut\n");
+    for(i=4;i>=0;i--)
+    {
+        printf("%d",d[i]);
+
+    }
+    printf("\n");
+    return 0;
+}
